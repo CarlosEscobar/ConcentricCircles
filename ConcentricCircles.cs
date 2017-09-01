b1c4b3a7de3c47eb024fc4caf2ad9feea98e5993
CarlosEscobar <cai_escobar@hotmail.com>
2017-09-01 14:10:48 -0600

    -First Commit

---
+++ ConcentricCircles.cs
@@ -0,0 +1,153 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Colors;
+using Autodesk.AutoCAD.Geometry;
+using System;
+
+[assembly: CommandClass(typeof(AutoCadAddIns.ConcentricCircles.ConcentricCircles))]
+
+namespace AutoCadAddIns.ConcentricCircles
+{
+    /// <summary>
+    /// Class defining commands for the concentric circles add in.
+    /// </summary>
+    public static class ConcentricCircles
+    {
+        /// <summary>
+        /// Correlative for BlockName.
+        /// </summary>
+        public static int BlockCorrelative = 1;
+        /// <summary>
+        /// Indicates the number of circles in the concentric circle.
+        /// </summary>
+        public static int numberOfCircles = 1;
+        /// <summary>
+        /// Factor to expand/contract the concentric circle.
+        /// </summary>
+        public static double radiusFactor = 1.0;
+        /// <summary>
+        /// X coordinate for the concentric circle center.
+        /// </summary>
+        public static double centerX = 0.0;
+        /// <summary>
+        /// Y coordinate for the concentric circle center.
+        /// </summary>
+        public static double centerY = 0.0;
+        /// <summary>
+        /// Array with RGB values for each circle.
+        /// </summary>
+        public static string[] circlesColors = { "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0",
+                                                 "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0",
+                                                 "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0",
+                                                 "0,0,0", "0,0,0", "0,0,0", "0,0,0", "0,0,0"};
+        
+        /// <summary>
+        /// Command to open the concentric circle form
+        /// </summary>
+        [CommandMethod("ConcentricCircles")]
+        public static void DoConcentricCircles()
+        {
+            //Show the Concentric Circles Form.
+            ConcentricCirclesForm concentricCirclesForm = new ConcentricCirclesForm();
+            Application.ShowModelessDialog(concentricCirclesForm);
+        }
+
+        /// <summary>
+        /// Command to draw the concentric circle
+        /// </summary>
+        [CommandMethod("DrawConcentricCircles")]
+        public static void DrawConcentricCircles()
+        {
+            //Get current autocad application document and database
+            Document currentDocument = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Database currentDatabase = currentDocument.Database;
+            //Open a database transaction
+            using (Transaction acadTransaction = currentDatabase.TransactionManager.StartTransaction())
+            {
+                //Using try catch to rollback and abort on error
+                try
+                {
+                    //Database table BlockTable opened in read mode
+                    BlockTable acadBlockTable;
+                    acadBlockTable = acadTransaction.GetObject(currentDatabase.BlockTableId, OpenMode.ForRead) as BlockTable;
+                    //Database table BlockTable opened in write mode
+                    BlockTableRecord acadBlockTableRecordWrite;
+                    acadBlockTableRecordWrite = acadTransaction.GetObject(acadBlockTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                    
+                    //Define block name
+                    string concentricCircleBlockName = "ConcentricCircle" + BlockCorrelative++;
+                    //Create Concentric Circle block
+                    BlockTableRecord concentricCircleBlockRecord = new BlockTableRecord();
+                    concentricCircleBlockRecord.Name = concentricCircleBlockName;
+                    //Upgrades the block table object to write mode
+                    acadBlockTable.UpgradeOpen();
+                    //Add Concentric Circle block to database
+                    ObjectId concentricCircleBlockId = acadBlockTable.Add(concentricCircleBlockRecord);
+                    acadTransaction.AddNewlyCreatedDBObject(concentricCircleBlockRecord, true);
+
+                    //Center of concentric circle
+                    Point3d center = new Point3d(centerX, centerY, 0.0);
+                    //Normal vector to circle
+                    Vector3d normal = new Vector3d(0, 0, 1.0);
+                    //Temporal circle variabe
+                    Circle acadCircle;
+                    //Temporal array to hold RGB values for circle
+                    string[] currentCircleColors;
+                    //Variables to hold object ids.
+                    ObjectIdCollection idFirstCircle = new ObjectIdCollection();
+                    ObjectIdCollection idLastCircle = new ObjectIdCollection();
+
+                    //Add circles
+                    for (int i = 1; i < (numberOfCircles + 1); i++)
+                    {
+                        using (acadCircle = new Circle(center, normal, i * radiusFactor))
+                        {
+                            //Get RGB values for circle
+                            currentCircleColors = circlesColors[i - 1].Split(',');
+                            acadCircle.Color = Color.FromRgb(Convert.ToByte(currentCircleColors[0]), Convert.ToByte(currentCircleColors[1]), Convert.ToByte(currentCircleColors[2]));
+                            //Define circle thickness
+                            acadCircle.Thickness = 3.0;
+                            //Add circle to database
+                            ObjectId circleId = concentricCircleBlockRecord.AppendEntity(acadCircle);
+                            acadTransaction.AddNewlyCreatedDBObject(acadCircle, true);
+                            //Store first and last circle ids for hatching boundaries
+                            if (i == 1) { idFirstCircle.Add(acadCircle.ObjectId); }
+                            if (i == numberOfCircles) { idLastCircle.Add(acadCircle.ObjectId); }
+                        }
+                    }
+                    
+                    //Add hatching
+                    using (Hatch acadHatch = new Hatch())
+                    {
+                        //Add hatch to database
+                        concentricCircleBlockRecord.AppendEntity(acadHatch);
+                        acadTransaction.AddNewlyCreatedDBObject(acadHatch, true);
+                        //Set hatch pattern
+                        acadHatch.SetHatchPattern(HatchPatternType.PreDefined, "ANSI31");
+                        acadHatch.Associative = true;
+                        //Set hatch boundaries (first and last circle)
+                        acadHatch.AppendLoop(HatchLoopTypes.Outermost, idFirstCircle);
+                        acadHatch.AppendLoop(HatchLoopTypes.Default, idLastCircle);
+                        acadHatch.EvaluateHatch(true);
+                    }
+                    
+                    //Define Concentric Circle Block Reference
+                    BlockReference concentricCircleBlockReference = new BlockReference(Point3d.Origin, concentricCircleBlockId);
+                    //Add Concentric Circle Block Reference to database
+                    acadBlockTableRecordWrite.AppendEntity(concentricCircleBlockReference);
+                    acadTransaction.AddNewlyCreatedDBObject(concentricCircleBlockReference, true);
+
+                    //Commit database changes.
+                    acadTransaction.Commit();
+                }
+                catch
+                {
+                    //Rollback and abort database changes on error.
+                    acadTransaction.Abort();
+                }
+            }
+        }
+
+    }
+}
---
+++ ConcentricCirclesForm.cs
@@ -0,0 +1,126 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using System;
+using System.Windows.Forms;
+
+namespace AutoCadAddIns.ConcentricCircles
+{
+    /// <summary>
+    /// Class defining Concentric Circles Form
+    /// </summary>
+    public partial class ConcentricCirclesForm : Form
+    {
+        
+        /// <summary>
+        /// Initialize Concentric Circles Form
+        /// </summary>
+        public ConcentricCirclesForm()
+        {
+            InitializeComponent();
+            InitializeCircles();
+        }
+
+        /// <summary>
+        /// Initialize numbers of circles combobox and circles list view
+        /// </summary>
+        private void InitializeCircles()
+        {
+            //Default value for concentric circle X coordinate
+            xOriginTextBox.Text = "0.0";
+            //Default value for concentric circle Y coordinate
+            yOriginTextBox.Text = "0.0";
+            //Default value for concentric circle radius factor
+            radiusFactorTextBox.Text = "1.0";
+            //Array with numbers from 1 to 20
+            string[] numbers = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                                 "11", "12", "13", "14", "15", "16", "17", "18", "19", "20" };
+
+            //Populate number of circles combo box
+            foreach (string number in numbers)
+            {
+                numberOfCirclesComboBox.Items.Add(number);
+            }
+            //Default value for number of circles
+            numberOfCirclesComboBox.SelectedIndex = 0;
+            
+            //Add circles list view for color picker
+            colorCircleListView.View = View.Details;
+            //Make list view multi select
+            colorCircleListView.MultiSelect = true;
+            colorCircleListView.HideSelection = false;
+            //Show checkboxes on each item
+            colorCircleListView.CheckBoxes = true;
+            colorCircleListView.HeaderStyle = ColumnHeaderStyle.Nonclickable;
+            //Define Column for 'Circle'
+            ColumnHeader circleColumnHeader = new ColumnHeader();
+            circleColumnHeader.Text = "Circle";
+            circleColumnHeader.TextAlign = HorizontalAlignment.Left;
+            circleColumnHeader.Width = 100;
+            //Define Column for 'Color'
+            ColumnHeader colorColumnHeader = new ColumnHeader();
+            colorColumnHeader.Text = "Color";
+            colorColumnHeader.TextAlign = HorizontalAlignment.Center;
+            colorColumnHeader.Width = 60;
+            //Add columns to list view
+            colorCircleListView.Columns.Add(circleColumnHeader);
+            colorCircleListView.Columns.Add(colorColumnHeader);
+            
+            //Populate list view and add default color for circles
+            foreach (string number in numbers)
+            {
+                ListViewItem listItem = new ListViewItem("Circle " + number);
+                listItem.SubItems.Add(" ");
+                listItem.UseItemStyleForSubItems = false;
+                listItem.SubItems[1].BackColor = System.Drawing.Color.Black;
+                colorCircleListView.Items.Add(listItem);
+            }
+        }
+
+        /// <summary>
+        /// Submit Concentric Circles Form
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Event arguments</param>
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            //Assign values from Concentric Circles Form
+            ConcentricCircles.numberOfCircles = Convert.ToInt32(numberOfCirclesComboBox.SelectedIndex+1);
+            ConcentricCircles.radiusFactor = Convert.ToDouble(radiusFactorTextBox.Text);
+            ConcentricCircles.centerX = Convert.ToDouble(xOriginTextBox.Text);
+            ConcentricCircles.centerY = Convert.ToDouble(yOriginTextBox.Text);
+
+            //Get current autocad application document
+            Document currentDocument = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            //Execute 'DrawConcentricCircles' command
+            currentDocument.SendStringToExecute(".DrawConcentricCircles ", true, false, false);
+        }
+
+        /// <summary>
+        /// Shows the color picker
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Event arguments</param>
+        private void colorButton_Click(object sender, EventArgs e)
+        {
+            //Define a color dialog
+            ColorDialog colorPickerDialog = new ColorDialog();
+            colorPickerDialog.AllowFullOpen = false;
+            colorPickerDialog.ShowHelp = true;
+            
+            if (colorPickerDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Update color picker button with latest color.
+                colorButton.BackColor = colorPickerDialog.Color;
+                
+                foreach (ListViewItem item in colorCircleListView.CheckedItems)
+                {
+                    //Sets new color for selected circles on list view
+                    ConcentricCircles.circlesColors[item.Index] = Convert.ToInt32(colorPickerDialog.Color.R) + "," + Convert.ToInt32(colorPickerDialog.Color.G) + "," + Convert.ToInt32(colorPickerDialog.Color.B);
+                    //Updates list view color graphically
+                    item.SubItems[1].BackColor = colorPickerDialog.Color;
+                }
+                //Refresh circles list view
+                colorCircleListView.Refresh();
+            }
+        }
+    }
+}
